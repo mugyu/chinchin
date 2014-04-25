e1008bf80b13d4c59bdf84fd59d7b483ed2b6632
mugyu <zcbyxvzz@gmail.com>
2014-04-25 18:21:04 +0900

    親の決定と、それに伴い親以外の参加者が子の組となる機能を追加
    fixed #3

--- lib/chinchin/game.rb
+++ lib/chinchin/game.rb
@@ -2,12 +2,34 @@
 
 module ChinChin
 
+  # ゲームを制御するクラス
   class Game
 
+    # プレイヤー(参加者)全員
     attr_reader :players
 
+    # 親(Banker)
+    attr_reader :banker
+
+    # 子の組(Punters)
+    attr_reader :punters
+
+    # @param [Array] players
+    #   ゲームのプレイヤー(参加者)を配列、または複数のパラ
+    #   メータとして渡す。
     def initialize(*players)
       @players = players.flatten
+    end
+
+    # 親を決定する
+    #
+    # 親が決まった時点で、
+    # それ以外のプレイヤーが子となる
+    #
+    # @param player 親にするプレイヤー
+    def banker=(player)
+      @punters = @players - [player]
+      @banker = player
     end
   end
 end
--- test/test_game.rb
+++ test/test_game.rb
@@ -17,4 +17,37 @@ class TestGame < Test::Unit::TestCase
     game = ChinChin::Game.new([player1, player2, player3])
     assert_equal [player1, player2, player3], game.players
   end
+
+  # 親(Banker)の設定と参照
+  def testSetBanker
+    player1 = Object.new
+    player2 = Object.new
+    player3 = Object.new
+
+    game = ChinChin::Game.new(player1, player2, player3)
+    game.banker = player2
+    assert_same player2, game.banker
+    assert_not_equal player1, game.banker
+    assert_not_equal player3, game.banker
+
+    # 親を新たに設定すると Game#bankerもそれに追随する
+    game.banker = player1
+    assert_same player1, game.banker
+  end
+
+  # 親(Banker)が決まったら、その他の参加者が子の組(punters)になる
+  def testPunters
+    player1 = Object.new
+    player2 = Object.new
+    player3 = Object.new
+
+    game = ChinChin::Game.new(player1, player2, player3)
+    game.banker = player2
+    assert_equal [player1, player3], game.punters
+
+    # 親が変わったら、新しい親が子の組から除外され、
+    # それまでの親が子の組に加わる
+    game.banker = player3
+    assert_equal [player1, player2], game.punters
+  end
 end
